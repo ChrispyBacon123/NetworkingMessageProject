5a65d7616ef4bd82937f5c31cdfb0f4c8091964e
ElijahSherman <161126881+ElijahSherman@users.noreply.github.com>
2024-02-24 14:24:22 +0200

    Add files via upload
    File containing the Client class to be used in the server

---
+++ ClientClass.py
@@ -0,0 +1,41 @@
+class Client:
+    
+    #Constructor to create new Client object
+    def __init__(self, username, password, status):
+        self.__username = username
+        self.__password = password
+        self.__status = status
+        self.__IP_add = ''
+        #The two leading underscores ensures the fields are private
+
+    #Accessor to get the username
+    def getName(self):
+        return self.__username
+    
+    #Accessor to get the password
+    def getPassword(self):
+        return self.__password
+    
+    #Accessor to get the user's status
+    def getStatus(self):
+        return self.__status
+    
+    #Accessor to get the IP address
+    def getIP(self):
+        return self.__IP_add
+
+    #Mutator to change the IP address
+    def changeIP(self, newIP):
+        self.__IP_add = newIP
+
+    #Mutator to change the password
+    def setPassword(self, new_pass):
+        self.__password = new_pass
+
+    #Mutator to change the user's visibility status
+    def setStatus(self, new_status):
+        self.__status = new_status
+
+    #Method to print out the information of a client for other clients to see
+    def toString(self):
+        return self.__username + ' ' + self.__status
