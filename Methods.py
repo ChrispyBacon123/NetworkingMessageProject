f2aa480d4791d70e7653990a70a05740ff07fa47
Christopher Blignaut <151723709+ChrispyBacon123@users.noreply.github.com>
2024-02-24 18:27:59 +0200

    Update Methods.py to have some methods to deal with message headers

--- Methods.py
+++ Methods.py
@@ -1,5 +1,7 @@
 import os
+from socket import *
 
+# Text file manipulation and client registering methods
 def unique_Username(name, clients):
     """Given the name, this method checks if the username is already taken by some else
         If the name is taken, themethod will return true, otherwise it will return falsex"""
@@ -39,9 +41,7 @@ def initialize_Clients():
             clients.append(client)
     return clients         
 
-
 def save_Client(client):
-
     """Saves the clients details into a text file on the server"""
     try:
         # File path to save details of the client
@@ -56,3 +56,105 @@ def save_Client(client):
 
     except IOError:
         print("The Client could not be saved")
+
+# Need to fix
+def main_Menu(option,connectionSocket):
+    """This method handles the main menu and all of it's cases.
+       It returns true if the method needs to be called again"""
+    
+    #Creating Header for the string
+
+    # Creating the main menu String
+    menu = menu +"\t\tMain menu:\n"
+    menu = menu +"1. Start a chat"
+    menu = menu +"2. Settings"
+    menu = menu +"3. Log Out"
+
+    # Option to send first time
+    if option == "":
+        connectionSocket.send(menu.encode())
+        return True
+
+    if option not in "123":
+        output = "Please choose option 1, 2 or 3\n"+menu
+        output = create_Header("M",output)
+        connectionSocket.send(output.encode())
+        return True
+    
+    # Printing the list of all the clients
+    if option == "1":
+        counter = 1
+        output = ""
+        for item in clientsList:
+            output = output+counter+" "+item.toString()+"\n"
+            counter+= 1
+        output = create_Header("M",output)
+        connectionSocket.send(output.encode())
+        return False
+    
+    if option == "2":
+        output = "Setting:\n1.Change Password\n2.Change Status"
+
+        return False
+
+    # The user has decided to log off
+    if option == "3":
+        output = "Thank you for using Disscord, Logging you off now!\nHave a nice day :)"
+        output = create_Header("X",output)
+        connectionSocket.send(output.encode())
+        connectionSocket.close()
+        return False
+
+
+def letter_Counter_Validation(size,body):
+    """Given the body of the message and the supposed number of characters in the message,
+    This method checks if the two correspond
+    This method will return true if the message passes the validation check"""
+    messageList = list(body)
+    if size == len(messageList):
+        return True
+    else:
+        return False
+    
+def create_Header(messageType,body):
+    """Given the type of message and the body, this method will return a header for the message
+    and return the message with the header attactched"""
+
+    # Attatching the header 
+    out = messageType
+
+    # Determining the size of the message
+    arr = list(body)
+    size = str(len(arr))
+
+    # Padding if the size of the message is less than 1000 characters
+    while(len(size)<4):
+        size = "0"+size
+    #Attatching the body
+    out = out+size+body
+    return out
+    
+
+    
+
+def main():
+    menu = "\t\tMain menu:\n"
+    menu = menu +"1. Start a chat"
+    menu = menu +"2. Settings"
+    menu = menu +"3. Log Out"
+
+    tear = list(menu)
+    print(len(tear))
+
+    size = "1049"
+
+    while(len(size)<4):
+        size = "0"+size
+    print(size)
+    number = int(size)
+    print(number)
+    print("this is needed for a git")
+
+
+if __name__ == "__main__":
+    main()
