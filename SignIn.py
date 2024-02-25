81010664ee4a0bad813675d7b624b1ac9de9a6f8
kmxmin <161126920+kmxmin@users.noreply.github.com>
2024-02-25 16:32:58 +0200

    Add files via upload
    Sign in method for the Server.py

---
+++ SignIn.py
@@ -0,0 +1,30 @@
+# returns True when signed in, False if it is not an existing account
+def sign_in(connectionSocket):
+    # asks client for a username
+    message = "USERNAME: "
+    message =create_Header("M",message)
+    connectionSocket.send(message.encode())
+
+    # gets the username
+    username = connectionSocket.recv(1024).decode()
+
+    userFound = False
+    
+    for client in clientsList:
+        if client.getName == username:
+            message = "PASSWORD: "
+            message =create_Header("M",message)
+            connectionSocket.send(message.encode())
+            password = connectionSocket.recv(1024).decode()
+
+            if client.getPassword == password:
+                return True
+            
+            userFound = True
+            break
+        
+    if userFound == False:
+        message = "Account not found. Create Datcord account :D\n"
+        connectionSocket.send(message.encode())
+        return False
+    
