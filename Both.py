10c9019df0ea6f6c7553b302771f6b73105be8c1
Christopher Blignaut <151723709+ChrispyBacon123@users.noreply.github.com>
2024-03-01 22:12:26 +0200

    Basic UDP (That I broke)

--- Both.py
+++ Both.py
@@ -2,20 +2,28 @@ import socket
 import threading
 import queue
 import ClientClass
+import time
 
+# USER_IP = '127.0.0.1'
+# SENDER_IP = '192.168.101.250'
+# NAME = ""
 
-messages =queue.Queue()
+SENDER_IP = '127.0.0.1'
+USER_IP = '192.168.101.250'
+NAME = ""
+sentCounter = 0
+messages = [" "] # Made the first message empty so the indexes would correspond to the message number 
+acknowledged = False
 # Makes port
 ipSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ipSocket.connect(("8.8.8.8", 80))
 ipAddress = ipSocket.getsockname()[0]
 IP = ipAddress
-print(IP)
 server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server.bind(('127.0.0.1',6969))
+server.bind((USER_IP,6969))
 
 
-def create_Header(messageType,body,chatNo):
+def create_Chat_Header(messageType,body,chatNo):
     """Given the type of message and the body, this method will return a header for the message
     and return the message with the header attactched"""
 
@@ -27,14 +35,34 @@ def create_Header(messageType,body,chatNo):
     size = str(len(arr))
 
     # Padding if the size of the message is less than 1000 characters
-    while(len(size)<4):
+    while len(size)<4:
         size = "0"+size
+
     # Padding if the size of the number is less than 1000
-    while (len(chatNo)<4):
-        chatNoS = "0"+chatNo
+    chatNoS=""+chatNo
+    while len(chatNoS)<4:
+        chatNoS = "0"+chatNoS
     #Attatching the body
     out = out+size+'*'+chatNoS+'*'+body
     return out
+
+def create_Header(messageType,body):
+    """Given the type of message and the body, this method will return a header for the message
+    and return the message with the header attactched"""
+    # Attatching the header 
+    out = messageType
+
+    # Determining the size of the message
+    arr = list(body)
+    size = str(len(arr))
+
+    # Padding if the size of the message is less than 1000 characters
+    while(len(size)<4):
+        size = "0"+size
+    #Attatching the body
+    out = out+size+body
+    return out
+
 
 def letter_Counter_Validation(size,body):
     """Given the body of the message and the supposed number of characters in the message,
@@ -46,42 +74,118 @@ def letter_Counter_Validation(size,body):
     else:
         return False
 
+def check_Message(message,size,body,chatNo):
+    """This method applies all the checks as per the protocol to ensure that
+    the entire message has been delivered uncorrupted it will return true if the message is correct"""
+    global recievedCounter
+    correct = False
+    # Checks to see if header got corrupted
+    if message[:1] not in "MISXARP":
+        mssg = create_Header("R",num)
+        correct=False
+    
+    # Checks to see if message number did not get corrupted
+    if message[11]!= "*" and message[:1]=="C":
+        correct=False
+        # mssg = create_Header("R",num)
+        # server.sendto(mssg.encode(),(SENDER_IP,6969))
+    # Checks to see if any messages were lost in their entirety
+    if recievedCounter!=chatNo and recievedCounter<chatNo:
+        correct = False
+        for i in range(recievedCounter,chatNo):
+            num = str(i)
+            while len(num)<4:
+                num= "0"+num
+            # mssg= create_Header("R",num)
+            # server.sendto(mssg.encode(),(SENDER_IP,6969))
+
+
+    # Checks to see if the body of the message was corrupted
+    if not letter_Counter_Validation(size,body):
+        # mssg = create_Header("R",chatNo)
+        # server.sendto(mssg.encode(),(SENDER_IP,6969))
+        correct = False
+    
+    return correct
+
+
+# Fix Resend Header
+def resend_Message(number):
+    """This method handles a resend message request, it takes the number of the message that needs to be resent and tries to resend the data"""
+    global acknowledged
+    global messages
+    found = False
+    counter =0
+    for i in range(3):
+        messages[number]
+        # Header is already included in the message 
+        message = message.getMessage()
+        server.sendto(message.encode(),((SENDER_IP,6969)))
+
+        # Message will always be found as the number will always be at most 1 less than the number of messages sent
+        time.sleep(3)
+        if acknowledged:
+            break
+        else:
+            counter+=1
+        if counter==3:
+            print("Failed to deliver message")
+    
+
 def recieve():
-   while True:
+    disconnect = False
+    global acknowledged
+    while not disconnect:
         try:
-            message, _ = server.recvfrom(1024)
-            message = message.decode()
-            # header = message[:5]
-            # chatNo = message[7:10]
-            # body = message[11:]
-            print(message)
-            # print(body)
-            # print(chatNo)
-            # print(header)
-            #print(letter_Counter_Validation())
+                message, _ = server.recvfrom(1024)
+                message = message.decode()
+                header = message[:5]
+                size = int(message[1:5])
+                chatNo = message[7:10]
+                body = message[11:]
+                if check_Message(message,size,body,chatNo):
+                    # We don't need to send an acknowledgement for an acknowledgement
+                    if header[:1] == 'A':
+                        acknowledged = True
+                    else:
+                        message = create_Chat_Header("A","",chatNo) # Sends acknowledgement for the chat number so that the sender knows which message this is for        
+                        server.sendto(message.encode(),(SENDER_IP,6969))
+                        print(body)
+                else: 
+                    resendThread = threading.Thread(target=resend_Message(chatNo)) 
+                    resendThread.start()
         except:
             pass
 
 def send():
-    while True:
+    global sentCounter
+    global messages
+    disconnect = False
+    while not disconnect:
         message = input("")
-        if message == "!q":
-            exit()  
+        if message == "DISCONNECT":
+            disconnect = True
+            message = create_Chat_Header("C",f"{NAME} has ended the chat",chatNo)         
+            server.sendto(message.encode(),(SENDER_IP,6969))
         else:
-            message = "cnsdMin: "+message
-            message = create_Header("C",message)
-            # server.sendto(f"{name}: {message}".encode(),("192.168.101.250",6969))
-            server.sendto(message.encode(),("192.168.101.250",6969))
+            # chatNo = str(sentCounter)
+            # while len(chatNo)<4:
+            #     chatNo="0"+chatNo
+            sentCounter+=1
+            chatNo = str(sentCounter)
 
+            # Adding message to the messages array 
+            message = create_Chat_Header("C",message,chatNo)      
+            messages.append(message)
+            server.sendto(message.encode(),(SENDER_IP,6969))
 
-# def resend_Request()
 
 def main():
-    t1 = threading.Thread(target=recieve)
-    t2 = threading.Thread(target=send)
+    recThread = threading.Thread(target=recieve)
+    sendThread = threading.Thread(target=send)
 
-    t1.start()
-    t2.start()
+    recThread.start()
+    sendThread.start()
     print("threads have started")
 
 if __name__=="__main__":
